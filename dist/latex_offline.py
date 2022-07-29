a45e224beb190673e6283af6ce7f85116ce8180c
Dimitri VINET <dimitri.vinet@edu.devinci.fr>
2022-07-29 09:55:25 +0200

    bumped version in cli

--- dist/latex_offline.py
+++ dist/latex_offline.py
@@ -8,7 +8,7 @@ from typing import List
 
 # Version dependent variables
 IM_NAME = "dimitrivinet/latex_offline"
-IM_VERSION_BASE = "v1.0.0"
+IM_VERSION_BASE = "v1.1.0"
 
 CONTAINER_NAME = "latex_offline"
 LOCAL_USER_ID = os.getuid()
